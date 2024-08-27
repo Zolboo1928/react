919a4e10a86579b6a20644f25cbe5456ea973d09
= <=>
2024-08-27 15:54:00 +0800

    bye

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -16,7 +16,7 @@ export default function RootLayout({ children }) {
       <body className={inter.className}>
         <Header/>
         {children}
-        <p>hello git!!!</p>
+        <p>bye git!!!</p>
         <Footer/>
         </body>
     </html>
